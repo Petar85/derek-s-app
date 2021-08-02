949e370f7907d216d70a96c971bfaa411a9a5975
Peter Pan <petarzivkovic85@gmail.com>
2021-08-01 22:41:17 -0700

    created hour array and added some padding and margin

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
-var a = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
              
-console.log(a);
+console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+// $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+$("#tdDate").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+var myContainer = $(".container-fluid")
+var hourArr = ["4:00:00AM","5:00:00AM","6:00:00AM","7:00:00AM","8:00:00AM","9:00:00AM","10:00:00AM","11:00:00AM","12:00:00PM","1:00:00PM","2:00:00PM","3:00:00PM","4:00:00PM","5:00:00PM","6:00:00PM","7:00:00PM","8:00:00PM","9:00:00PM","10:00:00PM","11:00:00PM","12:00:00AM","1:00:00AM","2:00:00AM","3:00:00AM",]
